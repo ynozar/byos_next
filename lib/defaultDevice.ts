744bf97b1fade57c5eafbd829aa05e6f22db2b9a
Mangle Kuo <ghcpuman902@gmail.com>
2025-03-03 23:32:23 +0000

    update default device

--- lib/defaultDevice.ts
+++ lib/defaultDevice.ts
@@ -1,9 +1,8 @@
 const defaultDevice = {
-                  FORLOG
-    friendly_id: "9A5DA8",
+    friendly_id: "FORLOG",
     device_name: "A TRMNL Device",
-    mac_address: "D8:3B:DA:F3:97:B4",
-    api_key: "LjKkY4L6QgpdQLyBzO9qdg",
+    mac_address: "AA:AA:AA:AA:AA:AA",   
+    api_key: "DefaultDeviceForLoggin",
     refresh_rate: 900,
   }
 
